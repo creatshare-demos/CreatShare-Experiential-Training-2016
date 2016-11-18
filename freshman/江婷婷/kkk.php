40375aa589ea71f7086178dea1a57a44bfa87b06
hylerrix <hylerrix@foxmail.com>
2016-11-18 23:17:22 +0800

    add

---
+++ freshman/江婷婷/kkk.php
@@ -0,0 +1,13 @@
+<html>
+
+<?php
+echo "<center>";
+echo "<img src=4.jpg>";
+echo "</center>"; 
+?>
+<?php echo $_POST["username"]; ?>
+<br>
+<?php echo $_POST["neirong"]; ?>
+<body>
+</body>
+</html>
